4b37d8dcfac4c41ba8378ef313239a09f78fb4ba
Emmurillo <emmanuel-ms@hotmail.com>
2015-09-28 16:39:24 -0600

    Add production and staging environments

--- gulpfile.js
+++ gulpfile.js
@@ -16,3 +16,35 @@ gulp.task('default', function(){
             console.log("Restarting...");
         })
 });
+
+gulp.task('staging', function(){
+    nodemon({
+        script: 'app.js',
+        ext: 'js',
+        env: {
+            PORT: 8000,
+            NODE_ENV: 'staging',
+            db: 'UVP_prod'
+        },
+        ignore: ['/node_modules/**']
+    })
+        .on('restart', function(){
+            console.log("Restarting...");
+        })
+});
+
+gulp.task('production', function(){
+    nodemon({
+        script: 'app.js',
+        ext: 'js',
+        env: {
+            PORT: 8000,
+            NODE_ENV: 'prod',
+            db: 'UVP_prod'
+        },
+        ignore: ['/node_modules/**']
+    })
+        .on('restart', function(){
+            console.log("Restarting...");
+        })
+});
