e15703f3765fe41ab2e02032c2470388220e41c7
Emmurillo <emmanuel-ms@hotmail.com>
2015-10-01 14:19:03 -0600

    Add users seeds for development

--- gulpfile.js
+++ gulpfile.js
@@ -7,44 +7,26 @@ gulp.task('default', function(){
         ext: 'js',
         env: {
             PORT: 8000,
-            NODE_ENV: 'dev',
             db: 'UVP_dev'
         },
         ignore: ['/node_modules/**']
     })
-        .on('restart', function(){
-            console.log("Restarting...");
-        })
+    .on('restart', function(){
+        console.log("Restarting...");
+    });
 });
 
-gulp.task('staging', function(){
+gulp.task('build', function(){
     nodemon({
         script: 'app.js',
         ext: 'js',
         env: {
             PORT: 8000,
-            NODE_ENV: 'staging',
-            db: 'UVP_prod'
+            db: 'UVPBuilder'
         },
         ignore: ['/node_modules/**']
     })
-        .on('restart', function(){
-            console.log("Restarting...");
-        })
-});
-
-gulp.task('production', function(){
-    nodemon({
-        script: 'app.js',
-        ext: 'js',
-        env: {
-            PORT: 8000,
-            NODE_ENV: 'prod',
-            db: 'UVP_prod'
-        },
-        ignore: ['/node_modules/**']
-    })
-        .on('restart', function(){
-            console.log("Restarting...");
-        })
+    .on('restart', function(){
+        console.log("Restarting...");
+    });
 });
