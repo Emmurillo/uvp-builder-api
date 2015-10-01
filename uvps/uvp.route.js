d4f4c87ddf07bd96ebd8920d7f0d899ea2d321ce
Emmurillo <emmanuel-ms@hotmail.com>
2015-10-01 16:26:08 -0600

    Add Update and GetAll endpoints

--- uvps/uvp.route.js
+++ uvps/uvp.route.js
@@ -4,9 +4,11 @@
   var Uvp = require('./uvp.controller')
 
   module.exports = function(app){
-    app.get('/uvps/user/:fb_id', Uvp.getAll);
+    app.get('/uvps/', Uvp.getAll);
+    app.get('/uvps/user/:fb_id', Uvp.getAllFromUser);
     app.get('/uvps/:uvp_id', Uvp.getOne);
+    app.put('/uvps/:uvp_id', Uvp.update);
     app.post('/uvps', Uvp.create);
-  }
+  };
 
 })();
